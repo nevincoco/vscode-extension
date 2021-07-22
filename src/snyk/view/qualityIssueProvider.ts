30203f013817d617803b611ce6e95ab855de2535
michelkaporin <michel.kaporin@snyk.io>
2021-07-22 18:25:36 +0200

    feat: add disabled message if config false

--- src/snyk/view/qualityIssueProvider.ts
+++ src/snyk/view/qualityIssueProvider.ts
@@ -1,8 +1,10 @@
 import { AnalyzerInterface } from '../../interfaces/SnykInterfaces';
+import { configuration } from '../configuration';
 import { ISnykCode } from '../lib/modules/code';
 import { IContextService } from '../services/contextService';
 import { IViewManagerService } from '../services/viewManagerService';
 import { IssueProvider } from './IssueProvider';
+import { Node } from './Node';
 
 export class CodeQualityIssueProvider extends IssueProvider {
   constructor(
@@ -12,5 +14,17 @@ export class CodeQualityIssueProvider extends IssueProvider {
     protected snykCode: ISnykCode,
   ) {
     super(viewManagerService, contextService, snykCode, analyzer.codeQualityReview);
+  }
+
+  getRootChildren(): Node[] {
+    if (!configuration.getFeaturesConfiguration()?.codeQualityEnabled) {
+      return [
+        new Node({
+          text: 'Snyk Code Quality is disabled. Enable it in settings to use it.',
+        }),
+      ];
+    }
+
+    return super.getRootChildren();
   }
 }
--- src/snyk/view/securityIssueProvider.ts
+++ src/snyk/view/securityIssueProvider.ts
@@ -1,8 +1,10 @@
 import { AnalyzerInterface } from '../../interfaces/SnykInterfaces';
+import { configuration } from '../configuration';
 import { ISnykCode } from '../lib/modules/code';
 import { IContextService } from '../services/contextService';
 import { IViewManagerService } from '../services/viewManagerService';
 import { IssueProvider } from './IssueProvider';
+import { Node } from './Node';
 
 export class CodeSecurityIssueProvider extends IssueProvider {
   constructor(
@@ -12,5 +14,17 @@ export class CodeSecurityIssueProvider extends IssueProvider {
     protected snykCode: ISnykCode,
   ) {
     super(viewManagerService, contextService, snykCode, analyzer.codeSecurityReview);
+  }
+
+  getRootChildren(): Node[] {
+    if (!configuration.getFeaturesConfiguration()?.codeSecurityEnabled) {
+      return [
+        new Node({
+          text: 'Snyk Code Security is disabled. Enable it in settings to use it.',
+        }),
+      ];
+    }
+
+    return super.getRootChildren();
   }
 }
